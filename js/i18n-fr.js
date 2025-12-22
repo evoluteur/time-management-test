e239d087af7f0b653cd2eaf377ea635210da7fb5
Olivier Giulieri <ogiulieri@gmail.com>
2025-12-21 21:08:11 -0800

    Move strings to i18n file + cleanups.

---
+++ js/i18n-fr.js
@@ -0,0 +1,51 @@
+// time-management-test
+// (c) 2025 Laurent Pellet
+
+const i18n = {
+  qs: [
+    "Je connais mes grands objectifs professionnels et personnels.",
+    "Je sais ce que mon entreprise attend de moi.",
+    "Mon agenda reflète mes priorités et intègre des créneaux pour mes sujets de fond.",
+    "Je me sens en contrôle de mon agenda",
+    "Je prends du plaisir en faisant mes tâches pro et perso.",
+    "Je connais mon biorythme et j'organise mes tâches en conséquence.",
+    "Je garde des marges de manœuvre pour absorber les imprévus.",
+    "Je sais me protéger des interruptions quand je dois me concentrer.",
+    'Je sais dire "non" aux sollicitations pour des tâches sans importance pour moi.',
+    "Je suis en maitrise et à jour de mes emails et messages.",
+    "Je consulte mes emails et messages à des moments bien définis.",
+    "Je ne procrastine pas et suis à jour de mes micro-tâches.",
+    "Je traite mes micro-tâches dans des plages dédiées.",
+    "Je délègue toutes les tâches pro et perso qui peuvent l'être.",
+    "Je challenge régulièrement ma façon de travailler et de m'organiser.",
+    "J'ai un équilibre Pro/perso qui me permet de rester serein(e) et performant(e) sur la durée",
+    "Je suis pleinement présent(e) dans ma vie personnelle et pour mes proches.",
+    "J'ai un sommeil de qualité et suffisant pour être en forme la journée.",
+    "J'ai des plages réservées pour mes activités sportives, sociales et familiales.",
+    "Je termine chaque journée en sachant ce que je ferai le lendemain.",
+  ],
+  rs: [
+    "Pas du tout d'accord",
+    "Plutôt pas d'accord",
+    "Neutre",
+    "Plutôt d'accord",
+    "Tout à fait d'accord",
+  ],
+  scores: [
+    "Votre gestion du temps semble très insuffisante. Il est fortement recommandé de revoir vos méthodes d'organisation et de planification pour améliorer votre efficacité et réduire le stress lié à une mauvaise gestion du temps.",
+    "Votre gestion du temps est insuffisante. Il serait bénéfique d'adopter de nouvelles stratégies d'organisation et de priorisation pour mieux gérer vos tâches et responsabilités.",
+    "Votre gestion du temps est moyenne. Vous pourriez améliorer votre efficacité en affinant vos techniques d'organisation et en vous concentrant davantage sur vos priorités.",
+    "Votre gestion du temps est bonne. Vous avez une approche efficace, mais il y a encore des opportunités pour optimiser davantage votre organisation et votre planification.",
+    "Votre gestion du temps est très bonne. Vous maîtrisez bien vos méthodes d'organisation et de planification, ce qui vous permet d'être efficace et productif dans vos activités quotidiennes.",
+    "Votre gestion du temps est excellente. Vous excellez dans l'organisation et la planification de vos tâches, ce qui vous permet d'optimiser votre productivité et de maintenir un équilibre sain entre vie professionnelle et personnelle.",
+  ],
+  intro:
+    "Évaluez chacune des affirmations ci-dessous en cochant une seule case par ligne.",
+  submit: "Calculer mon score",
+  more: "Envie de savoir comment améliorer votre score?",
+  more2:
+    'Retrouvons nous sur <a href="https://www.laurentpellet.com" target="_blank">www.laurentpellet.com</a>',
+  invalid:
+    "Veuillez répondre à toutes les questions. Il en reste $1 sans réponse.",
+  score: "Votre score total est de <span>$1%</span>.",
+};
---
+++ js/tmt.js
@@ -0,0 +1,94 @@
+// time-management-test
+// (c) 2025 Laurent Pellet
+
+const question = (q, idx) => {
+  return `<div id="q${idx}_wrapper" class="question">
+    <div class="qnum">${idx + 1}</div>
+    <div class="qq">
+      <div class="qtext">${q}</div>
+      <div class="qscale">
+        <label><input type="radio" name="q${idx}" value="0" onChange="javascript:setValid(${idx})"/>0</label>
+        <label><input type="radio" name="q${idx}" value="1" onChange="javascript:setValid(${idx})"/>1</label>
+        <label><input type="radio" name="q${idx}" value="2" onChange="javascript:setValid(${idx})"/>2</label>
+        <label><input type="radio" name="q${idx}" value="3" onChange="javascript:setValid(${idx})"/>3</label>
+        <label><input type="radio" name="q${idx}" value="4" onChange="javascript:setValid(${idx})"/>4</label>
+      </div>
+    </div>
+  </div>`;
+};
+
+let resShown = false;
+
+const setValid = (idx) => {
+  const elem = document.getElementById(`q${idx}_wrapper`);
+  elem.classList.remove("invalid");
+  if (resShown) {
+    showResults(false);
+  }
+};
+
+const calc = (withAlert = true) => {
+  let total = 0;
+  let invalids = 0;
+  for (let i = 0; i < i18n.qs.length; i++) {
+    const radios = document.getElementsByName(`q${i}`);
+    let invalid = true;
+    for (const r of radios) {
+      if (r.checked) {
+        invalid = false;
+        total += parseInt(r.value, 10);
+      }
+    }
+    const elem = document.getElementById(`q${i}_wrapper`);
+    if (invalid) {
+      invalids++;
+      elem.classList.add("invalid");
+    }
+  }
+  if (invalids && withAlert) {
+    setTimeout(() => {
+      alert(i18n.invalid.replace("$1", invalids));
+    }, 0);
+    total = -1;
+  }
+  return total;
+};
+
+const div = (txt) => `<div>${txt}</div>`;
+
+const showResults = (withAlert = true) => {
+  const total = calc(withAlert);
+  let h = "";
+  if (total > -1) {
+    const score = (100 * total) / 80;
+    h = div(i18n.score.replace("$1", score));
+    if (score < 10) {
+      h += div(i18n.scores[0]);
+    } else if (score < 30) {
+      h += div(i18n.scores[1]);
+    } else if (score < 50) {
+      h += div(i18n.scores[2]);
+    } else if (score < 70) {
+      h += div(i18n.scores[3]);
+    } else if (score < 90) {
+      h += div(i18n.scores[4]);
+    } else {
+      h += div(i18n.scores[5]);
+    }
+
+    h += div(i18n.more) + div(i18n.more2);
+    document.getElementById("score").innerHTML = h;
+    resShown = true;
+  }
+};
+
+const setup = () => {
+  const hRs = i18n.rs.map((r, idx) => `<div>${idx} = ${r}</div>`).join("");
+  document.getElementById("intro").innerHTML = div(i18n.intro) + div(hRs);
+
+  let hTest = i18n.qs.map(question).join("");
+  hTest += `<div class="submit">
+    <button onclick="showResults()">${i18n.submit}</button>
+  </div>`;
+  document.getElementById("test").innerHTML = hTest;
+};
